6ebd6b51e228dd4151849bf6da03a641ca1c09bb
Adamantios <adamantios.zaras@valory.xyz>
2023-09-18 21:06:24 +0300

    chore: bump trader service

--- run_service.sh
+++ run_service.sh
@@ -193,7 +193,7 @@ fi
 directory="trader"
 # This is a tested version that works well.
 # Feel free to replace this with a different version of the repo, but be careful as there might be breaking changes
-service_version="v0.6.0.post1"
+service_version="v0.6.1"
 service_repo=https://github.com/valory-xyz/$directory.git
 if [ -d $directory ]
 then
