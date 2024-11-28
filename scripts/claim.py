79c5efadf0df2eb05f4c9abf9ec6186d97809885
jmoreira-valory <jose.moreira.sanchez@valory.xyz>
2024-11-28 10:37:37 +0100

    fix: script

--- scripts/claim.py
+++ scripts/claim.py
@@ -20,16 +20,15 @@
 
 """Claim earned OLAS"""
 
-import argparse
-import os
-import requests
-import sys
-import textwrap
 import json
-from dotenv import dotenv_values, set_key, unset_key
+import os
+import sys
 from pathlib import Path
-from typing import Any, Dict, List, Tuple
+from typing import Any, Dict
+
+from dotenv import dotenv_values
 from web3 import Web3
+
 
 SCRIPT_PATH = Path(__file__).resolve().parent
 STORE_PATH = Path(SCRIPT_PATH, "..", ".trader_runner")
@@ -67,6 +66,7 @@ ERC20_ABI_PATH = Path(
     "ERC20.json",
 )
 
+
 def _load_abi_from_file(path: Path) -> Dict[str, Any]:
     if not os.path.exists(path):
         print(
@@ -80,33 +80,41 @@ def _load_abi_from_file(path: Path) -> Dict[str, Any]:
     return data.get("abi")
 
 
-
-
 def _erc20_balance(
     address: str,
     token_address: str = OLAS_TOKEN_ADDRESS_GNOSIS,
-    token_name: str = "OLAS"
-) -> int:
+    token_name: str = "OLAS",
+) -> str:
     """Get ERC20 balance"""
-    rpc = RPC_PATH.read_text().strip()
+    rpc = RPC_PATH.read_text(encoding="utf-8").strip()
     w3 = Web3(Web3.HTTPProvider(rpc))
     abi = _load_abi_from_file(ERC20_ABI_PATH)
     contract = w3.eth.contract(address=token_address, abi=abi)
     balance = contract.functions.balanceOf(address).call()
     return f"{balance / 10**18:.2f} {token_name}"
 
+
 def _claim_rewards() -> None:
+    service_safe_address = SERVICE_SAFE_ADDRESS_PATH.read_text(encoding="utf-8").strip()
+    print(
+        f"OLAS Balance on service Safe {service_safe_address}: {_erc20_balance(service_safe_address)}"
+    )
+
     env_file_vars = dotenv_values(DOTENV_PATH)
     staking_token_address = env_file_vars["CUSTOM_STAKING_ADDRESS"]
-    service_id = int(SERVICE_ID_PATH.read_text().strip())
+    service_id = int(SERVICE_ID_PATH.read_text(encoding="utf-8").strip())
 
-    rpc = RPC_PATH.read_text().strip()
+    rpc = RPC_PATH.read_text(encoding="utf-8").strip()
     w3 = Web3(Web3.HTTPProvider(rpc))
     abi = _load_abi_from_file(STAKING_TOKEN_IMPLEMENTATION_ABI_PATH)
     staking_token_contract = w3.eth.contract(address=staking_token_address, abi=abi)
 
-    owner_private_key = json.loads(OWNER_KEYS_JSON_PATH.read_text())[0]["private_key"]
-    owner_address = Web3.to_checksum_address(w3.eth.account.from_key(owner_private_key).address)
+    owner_private_key = json.loads(OWNER_KEYS_JSON_PATH.read_text(encoding="utf-8"))[0][
+        "private_key"
+    ]
+    owner_address = Web3.to_checksum_address(
+        w3.eth.account.from_key(owner_private_key).address
+    )
 
     function = staking_token_contract.functions.claim(service_id)
     claim_transaction = function.build_transaction(
@@ -119,18 +127,33 @@ def _claim_rewards() -> None:
     )
 
     signed_tx = w3.eth.account.sign_transaction(claim_transaction, owner_private_key)
-    tx_hash = w3.eth.send_raw_transaction(signed_tx.raw_transaction)
+    tx_hash = w3.eth.send_raw_transaction(signed_tx.rawTransaction)
     tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
-    print(f"Claim done. Receipt: {tx_receipt}")
+    print(f"Claim transaction done. Hash: {tx_hash.hex()}")
 
+    if "status" in tx_receipt and tx_receipt["status"] == 0:
+        print(
+            "The transaction was reverted. This may be caused because your service does not have rewards to claim."
+        )
+    else:
+        print("")
+        print(f"Claimed OLAS transferred to your service Safe {service_safe_address}")
+        print(
+            f"You can use your Owner/Operator wallet (address {owner_address}) to connect your Safe at"
+        )
+        print(f"https://app.safe.global/home?safe=gno:{service_safe_address}")
 
 
 def main() -> None:
     "Main method"
-    service_safe_address = SERVICE_SAFE_ADDRESS_PATH.read_text().strip()
+    print("This script will claim accrued OLAS on the current staking contract to your service Safe.")
+    response = input("Do you want to continue (yes/no)? ").strip().lower()
 
-    print(f"OLAS Balance on service Safe {service_safe_address}: {_erc20_balance(service_safe_address)}")
+    if response not in ("y", "yes"):
+        return
+
     _claim_rewards()
+
 
 if __name__ == "__main__":
     main()
