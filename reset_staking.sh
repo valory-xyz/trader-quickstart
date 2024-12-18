861215b9383156acb26d4f386ad5f9ae6bdf2713
OjusWiZard <ojuswimail@gmail.com>
2024-12-18 23:29:41 +0530

    refactor: move claim_olas and reset_staking into middleware
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- reset_staking.sh
+++ reset_staking.sh
@@ -1,15 +1,27 @@
 #!/bin/bash
 
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2023 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
 # force utf mode for python, cause sometimes there are issues with local codepages
 export PYTHONUTF8=1
 
+set -e  # Exit script on first error
 
-# Check if --attended flag is passed
-export ATTENDED=true
-for arg in "$@"; do
-  if [ "$arg" = "--attended=false" ]; then
-    export ATTENDED=false
-  fi
-done
-
-cd trader; poetry run python ../scripts/choose_staking.py --reset; cd ..
+poetry install --no-cache
+poetry run python -m operate.cli reset-staking config.json
--- run_service.sh
+++ run_service.sh
@@ -21,24 +21,7 @@
 # force utf mode for python, cause sometimes there are issues with local codepages
 export PYTHONUTF8=1
 
-# ------------------
-# Script starts here
-# ------------------
-
 set -e  # Exit script on first error
-
-# Initialize repo and version variables
-org_name="valory-xyz"
-directory="trader"
-service_repo=https://github.com/$org_name/$directory.git
-
-echo ""
-echo "==================="
-echo " Trader quickstart "
-echo "==================="
-echo ""
-echo "This script will assist you in setting up and running the Trader service ($service_repo)."
-echo ""
 
 # Display information of the Git repository
 current_branch=$(git rev-parse --abbrev-ref HEAD)
